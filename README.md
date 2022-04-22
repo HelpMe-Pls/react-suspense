a21df7da588acf28a084480ec591ea714af83f86
Random Guy <leekhoi5319990@gmail.com>
2022-04-22 13:39:44 +0700

    01_Render as you fetch

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # What I've learnt
-###### *For more details, see `src/exercise/*.md` files*
+###### *For more details, see [`src/exercise/*.md`](https://github.com/HelpMe-Pls/react-suspense/tree/master/src/exercise) files*
 -------------
+
+## Simple Data-fetching
+- Imagine when your app loads, you need some data **before** you can show anything useful. The best approaches to using `Suspense` involve kicking off the request for the data *as soon as* you have the information you need for the request. This is called the “**Render as you fetch**” approach.
--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,21 +3,22 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {PokemonDataView, fetchPokemon} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
+let pokemon
 
 // 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+// const pokemon = {
+//   name: 'TODO',
+//   number: 'TODO',
+//   attacks: {
+//     special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
+//   },
+//   fetchedAt: 'TODO',
+// }
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
@@ -25,32 +26,38 @@ const pokemon = {
 
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+const pokemonPromise = fetchPokemon('pikachu').then(
+	resolvedValue => (pokemon = resolvedValue),
+)
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+	// 🐨 if there's no pokemon yet, then throw the pokemonPromise
+	// 💰 (no, for real. Like: `throw pokemonPromise`)
+	if (!pokemon) throw pokemonPromise
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        <img src={pokemon.image} alt={pokemon.name} />
-      </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
+	// if the code gets it this far, then the pokemon variable is defined and
+	// rendering can continue!
+	return (
+		<div>
+			<div className="pokemon-info__img-wrapper">
+				<img src={pokemon.image} alt={pokemon.name} />
+			</div>
+			<PokemonDataView pokemon={pokemon} />
+		</div>
+	)
 }
 
 function App() {
-  return (
-    <div className="pokemon-info-app">
-      <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
-      </div>
-    </div>
-  )
+	return (
+		<div className="pokemon-info-app">
+			<div className="pokemon-info">
+				{/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
+				<React.Suspense fallback={<div>...loading</div>}>
+					<PokemonInfo />
+				</React.Suspense>
+			</div>
+		</div>
+	)
 }
 
 export default App
